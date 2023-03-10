820e1c80490ca5edc5986f7744fd7fca53bc9df2
hamidkunari <hamidkunari94@gmail.com>
2023-03-10 16:08:57 +0800

    FirstBasicCodePHP

---
+++ Iindex.php
@@ -0,0 +1,16 @@
+<?php
+// This is Just Basic Code of PHP
+//echo "PHP Gose Here";
+
+$name    = 'PHP Organization';
+$second  = 'PHP Development';
+$third   = 'PHP Developer';
+$fourth  = 'Laravel Development';
+
+echo $name;
+
+
+
+
+
+?>
